5b85a03a8be64477f125bfa28dbb8c71a7a795eb
Aaron Cox <aaronrcox123@gmail.com>
2024-12-04 14:03:27 +0800

    added day 04 solution

--- src/challenges/04/part1.ts
+++ src/challenges/04/part1.ts
@@ -5,10 +5,32 @@ export interface Day04P1RenderData {
     output: string;
 }
 
-function parseInput(input: string): string {
+function parseInput(input: string): string[] {
+    return input.split("\r\n");
+}
 
-    return input;
+function search(findText: string, input: string[], xPos: number, yPos: number, xDir: number, yDir: number): boolean {
+    let x = xPos;
+    let y = yPos;
+
+    for (let i = 0; i < findText.length; i++) {
+        // Check if current position is out of bounds
+        if (x < 0 || x >= input[0].length || y < 0 || y >= input.length) {
+            return false;
+        }
+        
+        // Check if current character matches
+        if (input[y][x] !== findText[i]) {
+            return false;
+        }
+        
+        // Move to the next position
+        x += xDir;
+        y += yDir;
+    }
     
+    // If we successfully matched all characters, return true
+    return true;
 }
 
 
@@ -19,8 +41,23 @@ export async function* solver(filePath: string): AsyncGenerator<Day04P1RenderDat
     const input = await readFileStr(filePath);
     const parsedInput = parseInput(input);
 
-    data.output = parsedInput;
+    const findText = "XMAS";
+    let found = 0;
+    for(let y=0; y<parsedInput.length; y++) {
+        for(let x=0; x<parsedInput[y].length; x++) {
 
+            if(search(findText, parsedInput, x, y, 1, 0)) found++; // search right
+            if(search(findText, parsedInput, x, y, 1, 1)) found++; // search right-down
+            if(search(findText, parsedInput, x, y, 0, 1)) found++; // search down
+            if(search(findText, parsedInput, x, y,-1, 1)) found++; // search down-left
+            if(search(findText, parsedInput, x, y,-1, 0)) found++; // search left
+            if(search(findText, parsedInput, x, y,-1,-1)) found++; // search left-up
+            if(search(findText, parsedInput, x, y, 0,-1)) found++; // search up
+            if(search(findText, parsedInput, x, y, 1,-1)) found++; // search up-right
+        }
+    }
+
+    data.output = `Found \"${findText}\" ${found} times`;
     yield data;
 }
 
--- src/challenges/04/part2.ts
+++ src/challenges/04/part2.ts
@@ -1,26 +1,67 @@
 import { runSolverWithAnimationFrame } from "../../utils/browser_renderer.ts";
 import { readFileStr } from "../../utils/input_reader.ts";
 
-export interface Day04P2RenderData {
+export interface Day04P1RenderData {
     output: string;
 }
 
-function parseInput(input: string): string {
+function parseInput(input: string): string[] {
+    return input.split("\r\n");
+}
 
-    return input;
+function search(findText: string, input: string[], xPos: number, yPos: number, xDir: number, yDir: number): boolean {
+    let x = xPos;
+    let y = yPos;
+
+    for (let i = 0; i < findText.length; i++) {
+        // Check if current position is out of bounds
+        if (x < 0 || x >= input[0].length || y < 0 || y >= input.length) {
+            return false;
+        }
+        
+        // Check if current character matches
+        if (input[y][x] !== findText[i]) {
+            return false;
+        }
+        
+        // Move to the next position
+        x += xDir;
+        y += yDir;
+    }
     
+    // successfully matched all characters
+    return true;
 }
 
 
-export async function* solver(filePath: string): AsyncGenerator<Day04P2RenderData> {
+export async function* solver(filePath: string): AsyncGenerator<Day04P1RenderData> {
 
-    const data: Day04P2RenderData = { output: "" };
+    const data: Day04P1RenderData = { output: "" };
 
     const input = await readFileStr(filePath);
     const parsedInput = parseInput(input);
 
-    data.output = parsedInput;
+    
+    let found = 0;
+    for(let y=0; y<parsedInput.length; y++) {
+        for(let x=0; x<parsedInput[y].length; x++) {
 
+            // Search for "MAS" or "SAM" in X Shape
+            // M.M || S.M || S.S || M.S
+            // .A. || .A. || .A. || .A.
+            // S.S || S.M || M.M || M.S
+
+            if( (search("MAS", parsedInput, x, y, 1, 1) && search("MAS", parsedInput, x+2, y,-1, 1)) ||  
+                (search("SAM", parsedInput, x, y, 1, 1) && search("MAS", parsedInput, x+2, y,-1, 1)) || 
+                (search("SAM", parsedInput, x, y, 1, 1) && search("SAM", parsedInput, x+2, y,-1, 1)) || 
+                (search("MAS", parsedInput, x, y, 1, 1) && search("SAM", parsedInput, x+2, y,-1, 1))
+            ) {
+                found++;
+            }
+        }
+    }
+
+    data.output = `Found "X-MAS" ${found} times`;
     yield data;
 }
 
@@ -30,7 +71,7 @@ export function loadRenderer(filepath: string) {
     runSolverWithAnimationFrame(solverSolutionStep, renderFrame);
 }
 
-function renderFrame(context: CanvasRenderingContext2D, data: Day04P2RenderData): void {
+function renderFrame(context: CanvasRenderingContext2D, data: Day04P1RenderData): void {
 
     // Clear the canvas
     context.clearRect(0, 0, context.canvas.width, context.canvas.height);
